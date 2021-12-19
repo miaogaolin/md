fe2594682edb16621ec4842f698d3f597fe9c1c1
苗高林 <2825873215@qq.com>
2021-12-19 19:40:42 +0800

    update notion proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,24 +1,25 @@
-// 该服务为 vercel serve跨域处理
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const request = require('request');
 
 module.exports = (req, res) => {
-  let target = ''
-
-  // 代理目标地址
-  // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
-  if (req.url.startsWith('/api')) {
-  // 这里填目标地址
-    target = 'https://api.notion.com'
+  // proxy middleware options
+  let prefix = "/notion-api"
+  if (!req.url.startsWith(prefix)) {
+    return;
   }
-  // 创建代理对象并转发请求
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/api`
-      // 例如 /api/user/login 将被转发到 http://target/user/login
-      '^/api/': '/'
-    }
-  })(req, res)
-}
+  let target = "https://api.notion.com" + req.url.substring(prefix.length);
 
+  var options = {
+    'method': 'GET',
+    'url': target,
+    'headers': {
+      'Notion-Version': '2021-08-16',
+      'Authorization': 'Bearer secret_pMvDSlhkNpmwLtrxMKgyF5ElOvBZdVCTDozrLzOUzCO'
+    }
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.write(response.body);
+    res.end();
+  });
+}
--- src/pages/index/view/CodemirrorEditor.vue
+++ src/pages/index/view/CodemirrorEditor.vue
@@ -217,7 +217,7 @@ export default {
       try {
         const notion = new Client({
           auth: auth,
-          baseUrl: "/notion-api"
+          baseUrl: "https://127.0.0.1/notion-api"
         });
         // passing notion client to the option
         const n2m = new notion2md({ notionClient: notion });
